080766892c14428e81923786869fd1002bbd836b
David-deng-yeah <1348478403@qq.com>
2023-09-27 23:07:57 +0800

    modify register

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
@@ -46,29 +46,55 @@ def logout(mysql):
 	return redirect(url_for('login'))
 
 def register(mysql):
-	msg = ''
-	if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
-		username = request.form['username']
-		password = request.form['password']
-		email = request.form['email']
-		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
-		account = cursor.fetchone()
-		if account:
-			msg = 'Account already exists !'
-		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-			msg = 'Invalid email address !'
-		elif not re.match(r'[A-Za-z0-9]+', username):
-			msg = 'Username must contain only characters and numbers !'
-		elif not username or not password or not email:
-			msg = 'Please fill out the form !'
-		else:
-			cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
-			mysql.connection.commit()
-			msg = 'You have successfully registered !'
-	elif request.method == 'POST':
-		msg = 'Please fill out the form !'
-	return render_template('register.html', msg = msg)
+    data = request.get_json()
+    if data and "username" in data and "password" in data and "email" in data:
+        username = data["username"]
+        password = data["password"]
+        email = data["email"]
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+        # Check if the username already exists
+        cursor.execute("SELECT * FROM accounts WHERE username = %s", (username,))
+        account = cursor.fetchone()
+
+        if account:
+            return jsonify({"msg": "Account already exists!"}), 400
+
+        # Add more validation checks here if needed (e.g., email format, password requirements)
+
+        # Insert the new user into the database
+        cursor.execute("INSERT INTO accounts (username, password, email) VALUES (%s, %s, %s)", (username, password, email))
+        mysql.connection.commit()
+
+        return jsonify({"msg": "success"}), 200
+    else:
+        return jsonify({"msg": "Please fill out the form!"}), 400
+
+# def register(mysql):
+# 	msg = ''
+# 	if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+# 		username = request.form['username']
+# 		password = request.form['password']
+# 		email = request.form['email']
+# 		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+# 		cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+# 		account = cursor.fetchone()
+# 		if account:
+# 			msg = 'Account already exists !'
+# 		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+# 			msg = 'Invalid email address !'
+# 		elif not re.match(r'[A-Za-z0-9]+', username):
+# 			msg = 'Username must contain only characters and numbers !'
+# 		elif not username or not password or not email:
+# 			msg = 'Please fill out the form !'
+# 		else:
+# 			cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+# 			mysql.connection.commit()
+# 			msg = 'You have successfully registered !'
+# 	elif request.method == 'POST':
+# 		msg = 'Please fill out the form !'
+# 	return render_template('register.html', msg = msg)
 
 
 
