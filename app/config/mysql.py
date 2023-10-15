31e581ff38892bce4574aeb927b110550ae4a8dd
David-deng-yeah <1348478403@qq.com>
2023-10-15 17:26:37 +0800

    modify mysql configuration with YAML

--- app/config/mysql.py
+++ app/config/mysql.py
@@ -1,42 +1,19 @@
-# class DbConfig:
-#     def __init__(self, driver_name, dsn, show_sql, show_exec_time, max_idle, max_open):
-#         self.driver_name = driver_name
-#         self.dsn = dsn
-#         self.show_sql = show_sql
-#         self.show_exec_time = show_exec_time
-#         self.max_idle = max_idle
-#         self.max_open = max_open
-
-# class DbConfig:
-#     def __init__(self, host_name, user_name, password, database_name, show_sql, show_exec_time, max_idle, max_open):
-#         self.host = host_name
-#         self.username = user_name
-#         self.password = password
-#         self.database = database_name
-#         self.show_sql = show_sql
-
-# # 创建数据库配置字典
-# Db = {
-#     "db1": DbConfig(
-#         host_name="localhost",
-#         user_name="dev",
-#         password="dev",
-#         database_name="userdata",
-#         show_sql=True,
-#         # show_exec_time=False,
-#         # max_idle=10,
-#         # max_open=200
-#     )
-# }
-# mysql_config.py
-
+import yaml
+import os
 from flask_mysqldb import MySQL
 
-def configure_mysql(app):
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = '20010127Hyq*'
-    app.config['MYSQL_DB'] = 'user_data'
-    app.config['SECRET_KEY'] = 'afhfhkgigugh'
+def load_config():
+    current_directory = os.getcwd()
+    config_file_path = os.path.join(current_directory, 'config', 'config.yaml')
+    with open(config_file_path, "r") as config_file:
+        config = yaml.safe_load(config_file)
+    return config
+
+def configure_mysql(app, mysql_config):
+    app.config['MYSQL_HOST'] = mysql_config['host']
+    app.config['MYSQL_USER'] = mysql_config['user']
+    app.config['MYSQL_PASSWORD'] = mysql_config['password']
+    app.config['MYSQL_DB'] = mysql_config['db']
+    app.config['SECRET_KEY'] = mysql_config['secret_key']
     mysql = MySQL(app)
     return mysql
--- app/run.py
+++ app/run.py
@@ -1,17 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-import json
-# from flask_cors import CORS
-# from functools import wraps
-# import jwt
-# import url
-# import conf
-# import common
-# import response
-# import app
-
-# import sys
-# sys.path.append("/home/hewen/ISS-workshop/MVP/AnimeRecommendation_backend/app")
 
 
 from routes import users
@@ -20,12 +8,12 @@ from routes import anime
 from config import mysql
 #from routes import chatbot
 
-# app = Flask(__name__)
-# app = Flask(__name__, template_folder="static/templates")  # Update the template_folder path
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'content-type'
-mysql = mysql.configure_mysql(app)
+app_config = mysql.load_config()
+mysql = mysql.configure_mysql(app, app_config['mysql'])
+# mysql = mysql.configure_mysql(app)
 
 # def load():
 #     c = conf.Config()
@@ -48,19 +36,12 @@ def register():
 @app.route("/login", methods=["OPTIONS","GET","POST"])
 def login():
     return users.login(mysql)
-# register route
 
  
 
 @app.route("/logout")
-# @auth_required
 def logout():
     return users.logout(mysql)
-# @app.route("/my/info", methods=["GET"])
-# # @auth_required
-# def get_user_info():
-#     # TODO
-#     pass
 
 #Anime fetch from database
 @app.route('/anime', methods=['GET'])
@@ -86,44 +67,6 @@ def get_user_ratings(account_id, anime_id):
 def rate_anime():
     print('upload score')
     return rating.upload_user_ratings(mysql)
-
-
-
-#@app.route('/getAnime', methods=['GET'])
-#def fetch_anime_by_keyword():
-#    keyword = request.args.get('keyword', 'One Piece')
-#    page = int(request.args.get('page', 1))
-#    return get_anime_by_keyword(mysql, keyword, page)
-
-#using ID
-#@app.route('/getAnimeByID', methods=['GET'])
-#def fetch_anime_by_id():
- #   anime_id = request.args.get('id')
- #   if not anime_id:
- #       return jsonify({"msg": "Please provide an Anime ID!"}), 400
-  #  return get_anime_by_id(mysql, anime_id)
-
-
-
-# TODO
-# @app.route("/rating", methods=["POST"])
-# # @auth_required
-# def rating():
-#     # TODO
-#     pass
-
-# @app.route("/rating/getAll", methods=["POST"])
-# # @auth_requiredshu
-# def get_all_rating():
-#     # TODO
-#     pass
-
-# @app.route("/rating/rateAMovie", methods=["POST"])
-# # @auth_required
-# def rate_a_movie():
-#     # TODO
-#     pass
-
 
 if __name__ == '__main__':
     # load()
